2d134c90c99ec910a2e26b35cc81c271e2eed15e
Julius Caesar <juliuscaesarlaksana@gmail.com>
2023-04-06 19:59:48 +0700

    Added HTTP put & delete request on /users/:id

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/ping", (req, res) => {
-  res.send("Pong");
+  res.send({ success: true, });
 });
 
 app.get("/users", (req, res) => {
@@ -35,8 +35,7 @@ app.post("/users", (req, res) => {
 
   const result = schema.validate(req.body);
   if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
+    return res.status(400).send(result.error.details[0].message);
   }
 
   const user = {
@@ -47,12 +46,44 @@ app.post("/users", (req, res) => {
   res.send(user);
 });
 
+app.put("/users/:id", (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).json({ success: false });
+  }
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+
+  const result = schema.validate(req.body);
+  if (result.error) {
+    return res.status(400).send(result.error.details[0].message);
+  }
+  
+  user.name = req.body.name
+  res.send(user);
+
+});
+
 app.get("/users/:id", (req, res) => {
   const user = users.find(u => u.id === parseInt(req.params.id));
   if (!user) {
     return res.status(404).json({ success: false });
   }
   res.send(trueResponse(user));
+});
+
+app.delete("/users/:id", (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).json({ success: false });
+  }
+
+  const indexUsers = users.indexOf(user);
+  users.splice(indexUsers, 1);
+
+  res.send(user);
 });
 
 app.listen(3000, () => console.log("Listening to 3000.."));
